1e6c630a0da9865863f8fd25d184e8097bdc5bf3
ReekyStive <reekystive@gmail.com>
2023-10-14 17:01:01 +0800

    deps: 📦 add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,10 @@ const { defineConfig } = require('eslint-define-config');
 module.exports = defineConfig({
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  plugins: ['prettier'],
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },

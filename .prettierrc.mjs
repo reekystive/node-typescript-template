5def4e0df63cfca3d1fb235b0ad3c53faad0fedf
ReekyStive <reekystive@gmail.com>
2023-10-17 00:22:47 +0800

    chore: 👀 use mjs config for prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+export default {
+  semi: true,
+  printWidth: 120,
+  trailingComma: 'es5',
+  singleQuote: true,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-organize-imports'],
+};

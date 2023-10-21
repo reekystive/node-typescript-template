363a5d172cb50518a835ec79a85943e21b76111b
Mingxuan Wang <reekystive@gmail.com>
2023-10-22 00:29:06 +0800

    feat(koa): ✨ add koa branch

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-console.log('hello world!');
+import cors from '@koa/cors';
+import Router from '@koa/router';
+import Koa from 'koa';
 
-export {};
+const app = new Koa();
+const router = new Router();
+
+app.use(cors({ origin: '*' }));
+
+router.get('/', async (ctx) => {
+  ctx.body = 'Hello World!';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT ?? 3000;
+console.log(`Server is running on http://127.0.0.1:${port}/`);
+app.listen(port);
